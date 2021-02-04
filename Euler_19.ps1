86b34c7a0935f29eb9619ef0671ac5e995ff9331
Evmor <69753854+Evmor@users.noreply.github.com>
2021-02-04 07:52:34 +0300

    Add files via upload

---
+++ Euler_19.ps1
@@ -0,0 +1,16 @@
+﻿# Посчитать, сколько воскресений
+# было на 1-е число месяца 
+# с 01.01.1901 по 31.12.2000
+# 
+# Ответ: 171
+
+$SundayCount = 0
+for ($year = 1901; $year -le 2000; $year++) {
+    for ($month = 1; $month -le 12; $month++) {
+        if ((get-date -Year $year -Month $month -Day 01).DayOfWeek -eq 'Sunday') {
+            $SundayCount++
+        }
+    }
+}
+
+Write-Host "Воскресений было: $SundayCount"
---
+++ Euler_22.ps1
@@ -0,0 +1,64 @@
+﻿# Задача решена.
+# Сумма очков имен равна 871198282
+
+$string = Get-Content .\Downloads\names.txt
+$string = $string -replace '"'
+$array = $string -split ','
+$array = $array | sort
+$hashtable = @{}
+$hashtableABC = @{}
+$array | ForEach-Object -Begin {$i = 1} -Process {
+    $hashtable.$i = $PSItem
+    $i++
+}
+
+$TheABC = (65..90) | ForEach-Object {[string] [char] $PSItem}
+
+$TheABC | ForEach-Object -Begin {$i = 1} -Process {
+    $hashtableABC.$PSItem = $i
+    $i++
+}
+
+# Сумма очков имени
+# Функция, на входе получает имя
+function NameBalls {
+    param ($name)
+    $NameCount = 0
+    $name = $name -split ''
+    $name | ForEach-Object {
+        if ($PSItem -in $hashtableABC.Keys) {
+            $NameCount += [int] $hashtableABC.$($PSItem)
+        }
+    }
+    $NameCount
+}
+
+function MainFunction {
+    $SumNames = 0
+    for ($i=1; $i -le $hashtable.Count; $i++) {
+        $SumNames += ($i * (NameBalls $hashtable.$i))
+    }
+    Write-Host "Сумма очков имен равна $SumNames"
+}
+
+# Проверка тестового условия
+# 'COLIN' - 938 в списке
+# Сумма очков имени = 53
+# Имя 'COLIN' получает 938*53=49714 очков.
+# Какова сумма очков имен в файле?
+If ($hashtable.938 -ne 'COLIN') {
+    Write-Host "Тестовое имя $($hashtable.938) не подходит" -ForegroundColor Red
+    break
+} else {
+    Write-Host "Тестовое имя $($hashtable.938) подходит." -ForegroundColor Green
+    if ((NameBalls $hashtable.938) -ne 53) {
+        Write-Host "Сумма очков имени $($hashtable.938) не подходит." -ForegroundColor Red
+    } else {
+        Write-Host "Сумма очков имени $($hashtable.938) правильная." -ForegroundColor Green
+        MainFunction
+    }
+}
+
+
+
+
