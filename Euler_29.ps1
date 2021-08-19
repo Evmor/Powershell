253c8d1f09b54d90766a6ab1b4e539e8c461c181
Evmor <69753854+Evmor@users.noreply.github.com>
2021-08-19 15:29:50 +0300

    Add files via upload

---
+++ Euler_29.ps1
@@ -0,0 +1,18 @@
+﻿# 'a'  в степени  'b'
+# при 2 <= a <= 100
+# и 2 <= b <= 100
+
+
+Write-Host 'Время на выполнение:'
+Measure-Command -Expression {
+    $array = @()
+    for ($a=2; $a -le 100; $a++) {
+        for ($b=2; $b -le 100; $b++){
+            $array += [math]::Pow($a, $b)
+            $count++
+        }
+    }
+    $array = $array | Select-Object -Unique
+}
+
+Write-Host "Правильный ответ: " $array.Count
