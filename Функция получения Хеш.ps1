44668dcd9d0c3fdf0ef1ad20e651d0d86426df45
Evmor <69753854+Evmor@users.noreply.github.com>
2021-08-19 11:53:25 +0300

    Add files via upload

---
+++ Функция получения Хеш.ps1
@@ -0,0 +1,19 @@
+﻿function Get-HashFiles {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        $Path,
+        [Parameter(Mandatory)]
+        [ValidateSet('MACTripleDES','MD5','RIPEMD160','SHA1','SHA256','SHA384','SHA512')]
+        $Algorithm
+    )
+    $hash = @{}
+    $files = ls $Path -File -Recurse
+    $fileHash = $files | ForEach-Object {Get-FileHash $PSItem.FullName -Algorithm $Algorithm}
+    foreach ($file in $fileHash) {
+        $hash += @{
+            $file.Path=$file.Hash
+        }
+    }
+    $hash
+}
